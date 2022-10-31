19540d16c1f608800b6359ba561c5b93de863e26
Andrey <andreysyrotenko@gmail.com>
2022-10-31 22:31:03 +0200

    Task 3 fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,13 +6,7 @@ const refs = {
 
 const FEEDBACK = 'feedback-form-state';
 
-let formData = {};
-
-const text = JSON.parse(localStorage.getItem(FEEDBACK));
-
-if (text) {
-  formData = { ...text };
-}
+let formData = JSON.parse(localStorage.getItem(FEEDBACK)) || {};
 
 populateText();
 
@@ -30,49 +24,14 @@ refs.form.addEventListener('submit', e => {
 
   e.target.reset();
   localStorage.removeItem(FEEDBACK);
+  formData = {};
 });
 
 function populateText() {
   const savedText = JSON.parse(localStorage.getItem(FEEDBACK));
 
-  // if (savedText) {
-  //   if (savedText.email) {
-  //     refs.form.email.value = savedText.email;
-  //   }
-  //   if (savedText.message) {
-  //     refs.form.message.value = savedText.message;
-  //   }
-  // }
-
-  // const keys = Object.keys(savedText);
-  // console.log(keys);
-
-  // if (savedText) {
-  //   // for (let index = 0; index < keys.length; index += 1) {
-  //   //   console.log(refs.form.keys[index].value);
-  //   // }
-  //   // for (const key of keys) {
-  //   //   console.log(refs.form.key);
-  //   //   // refs.form.key = savedText[key];
-  //   // }
-  //   keys.forEach(function callback(e, i, a) {
-  //     console.log(refs.form.e);
-  //   });
-  // }
-
-  try {
-    if (savedText.email) {
-      refs.form.email.value = savedText.email;
-    }
-  } catch (error) {
-    return;
-  }
-
-  try {
-    if (savedText.message) {
-      refs.form.message.value = savedText.message;
-    }
-  } catch (error) {
-    return;
+  if (savedText) {
+    refs.form.email.value = savedText.email || '';
+    refs.form.message.value = savedText.message || '';
   }
 }
